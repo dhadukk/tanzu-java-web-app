b7e797b94d4b3bb32a8c3eb75f86cb782773ce8c
kinjal dhaduk <38214481+dhadukk@users.noreply.github.com>
2021-12-08 11:23:09 +0530

    Changed msg

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot + Tanzu1!";
+		return "Greetings from Spring Boot + Tanzu + TAP!";
 	}
 
 }
